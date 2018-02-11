78e5073517f56805743dfeedc695072e402dd45b
Mikko Nylén <mikko@mikkonylen.fi>
2018-02-11 15:46:03 +0200

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-statistics-fns
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clj_statistics_fns/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-statistics-fns.core-test
+  (:require [clojure.test :refer :all]
+            [clj-statistics-fns.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
